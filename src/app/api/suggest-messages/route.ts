da72d2cf2e9b803c578dd140a1123ca105619557
dkcoder02 <krishdesai044@gmail.com>
2024-05-19 11:37:00 +0530

    refactor: Update MistralClient import and function parameter types in suggest-messages API route

--- src/app/api/suggest-messages/route.ts
+++ src/app/api/suggest-messages/route.ts
@@ -1,10 +1,12 @@
-import MistralClient from "@mistralai/mistralai";
+import MistralClient, {
+  ChatCompletionResponseChunk,
+} from "@mistralai/mistralai";
 import { MistralStream, StreamingTextResponse } from "ai";
-import { ChatCompletionResponseChunk } from "@mistralai/mistralai";
+import { NextRequest, NextResponse } from "next/server";
 
 const mistral = new MistralClient(process.env.MISTRAL_API_KEY || "");
 
-export async function POST(req: Request) {
+export async function POST(req: NextRequest) {
   try {
     const prompt =
       "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
